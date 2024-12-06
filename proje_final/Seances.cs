d88c22a97c429798ea6d2aba4bd06c8852d5532e
mazong dongmo <manuel.glory.mazong.dongmo.01@edu.cegeptr.qc.ca>
2024-12-06 12:40:35 -0500

    premiere partie sur les seances

---
+++ proje_final/Seances.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje_final
+{
+    internal class Seances : INotifyPropertyChanged
+    {
+        public int id;
+        public int idActivite;
+        public string nomActivite;
+        public string dateOrganisation;
+        public string heureOrganisation;
+        public int nombrePlace;
+        public int nombrePlaceRestante;
+
+        public int Id { get => id; set { id = value; } }
+        public int IdActivite { get=> idActivite; set { idActivite = value; OnPropertyChanged(nameof(IdActivite)); } }
+        public string DateOrganisation { get => dateOrganisation; set { dateOrganisation = value; OnPropertyChanged(nameof(DateOrganisation)); } }
+        public string HeureOrganisation { get => heureOrganisation; set { heureOrganisation = value; OnPropertyChanged(nameof(HeureOrganisation)); } }
+        public int NombrePlace { get => nombrePlace; set { nombrePlace = value; OnPropertyChanged(nameof(NombrePlace)); } }
+        public int NombrePlaceRestante { get => nombrePlaceRestante; set { nombrePlaceRestante = value; OnPropertyChanged(nameof(NombrePlaceRestante)); } }
+        public string NomActivite { get => nomActivite; set { nomActivite = value; OnPropertyChanged(nameof(NomActivite)); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
